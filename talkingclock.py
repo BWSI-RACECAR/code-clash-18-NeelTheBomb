7e6c2f783433b5cb8872eef07aed653036e668a5
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-08-04 09:05:04 -0400

    Create talkingclock.py

---
+++ talkingclock.py
@@ -0,0 +1,18 @@
+class Solution:    
+    def ClockTalker(self, input_time):
+            #type input_time: string
+            #return type: string
+            print = input_time
+            
+            #TODO: Write code below to return a string with the solution to the prompt.
+            pass
+
+def main():
+     str1=input()
+     tc1= Solution()
+     ans=tc1.ClockTalker(str1)
+     print(ans)
+    
+if __name__ == '__main__': 
+    main()
+        
