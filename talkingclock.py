60475893212ef982fa8f77f9f2423d020ef5b25e
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-08-04 09:08:07 -0400

    Update talkingclock.py

--- talkingclock.py
+++ talkingclock.py
@@ -1,8 +1,9 @@
 class Solution:    
     def ClockTalker(self, input_time):
+            first_dict = {"1": "one:am", "2":"two:am", "3":"three:am", "4":"four:am":"5":"five:am"} 
             #type input_time: string
             #return type: string
-            print(input_time)
+            print(type(input_time))
             
             #TODO: Write code below to return a string with the solution to the prompt.
             pass
